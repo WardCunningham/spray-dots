24fbf662d68dd70224954257b9185b227ce1b020
Ward Cunningham <ward@c2.com>
2015-08-15 09:25:39 -0700

    add cluster left/right

--- fabricate.js
+++ fabricate.js
@@ -1,17 +1,14 @@
 function fabricate (opt) {
-  var scatter = d3.random.normal(0,opt.scatter)
   var data = []
 
   d3.range(25).map(function () {
     data.push({
       color: 'blue',
-      x: width/4 + scatter(),
-      y: height/2 + scatter()
+      cluster: 'left'
     })
     data.push({
       color: 'red',
-      x: width*3/4 + scatter(),
-      y: height/2 + scatter()
+      cluster: 'right'
     })
   })
 
