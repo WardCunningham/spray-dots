81cfc5833dfb2d1aabe608d284f33e6f71f67cb7
Ward Cunningham <ward@c2.com>
2015-08-16 21:34:18 -0700

    move spray to separate file (2 of 2)

--- spray.js
+++ spray.js
@@ -3,6 +3,7 @@
 function sprayChart (opt) {
   var width = opt.width || 900
   var height = opt.height || 400
+  var radius = opt.radius || function (d) {return d.r}
   var force = d3.layout.force()
 
   function spray (selection) {
@@ -41,6 +42,53 @@ function sprayChart (opt) {
           .call(dots)
       }
 
+      function dots (selection) {
+        selection
+          .attr('cx', function (d) {return d.x})
+          .attr('cy', function (d) {return d.y})
+          .attr('r',radius)
+          .attr('fill', function (d) {return d.color})
+      }
+
+      function gravity(alpha) {
+        var y = height/2
+        return function(d) {
+          var x = d.cluster == 'left' ? width/4 : width*3/4
+          d.y += (y - d.y) * alpha
+          d.x += (x - d.x) * alpha
+        }
+      }
+
+      function collide(alpha) {
+        var quadtree = d3.geom.quadtree(data)
+        var padding = 2
+        return function(d) {
+          var r = radius(d) + padding,
+              nx1 = d.x - r,
+              nx2 = d.x + r,
+              ny1 = d.y - r,
+              ny2 = d.y + r
+          quadtree.visit(function(quad, x1, y1, x2, y2) {
+            if (quad.point && (quad.point !== d)) {
+              var x = d.x - quad.point.x,
+                  y = d.y - quad.point.y,
+                  l = Math.sqrt(x * x + y * y),
+                  r = radius(d) + radius(quad.point) + padding
+              if (l < r) {
+                l = (l - r) / l * alpha;
+                d.x -= x *= l;
+                d.y -= y *= l;
+                quad.point.x += x
+                quad.point.y += y
+              }
+            }
+            return x1 > nx2
+                || x2 < nx1
+                || y1 > ny2
+                || y2 < ny1
+          })
+        }
+      }
     })
   }
 
