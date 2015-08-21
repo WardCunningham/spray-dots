01731e574fa3203fbea9b8a6d5e56c2b3cecb824
Ward Cunningham <ward@c2.com>
2015-08-21 08:04:52 -0700

    move random selection to fabricate file

--- fabricate.js
+++ fabricate.js
@@ -14,3 +14,18 @@ function fabricate (opt) {
 
   return data
 }
+
+function any (array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+function favor (data, favor) {
+  var mobile = data.filter(function (d) {return d.color == favor})
+  return data.concat(mobile,mobile,mobile)
+}
+
+function lean (choice, leaning, count) {
+  for (var i=0; i<count; i++) {
+    any(choice).cluster = any(leaning)
+  }
+}
